d6847923790996b5b714175d921a84adc266496a
Manish Lal <manishslal@Manishs-MacBook-14.local>
2025-11-16 20:53:42 -0500

    Centralize db initialization in models/__init__.py and update model imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
+# Centralized SQLAlchemy db instance
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+from .user import User
+from .bet import Bet
+from .bet_leg import BetLeg
 # models package init
